b91296126138961183a79bc451f79875bb759ba9
MichaelMontei <code-me@hotmail.com>
2022-05-04 14:40:24 +0200

    not sure if alert or we should use the explain class here

--- 01-base/02-var-prompt/script.js
+++ 01-base/02-var-prompt/script.js
@@ -9,8 +9,12 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+let nickname = prompt("Enter your name");
+alert("Hello, " + nickname + "!");
+
+
+
 (function() {
 
-    // your code here
 
 })();
