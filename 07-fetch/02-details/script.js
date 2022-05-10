eac8762c2643e70071958c65f1cec905ec5f95b0
MichaelMontei <code-me@hotmail.com>
2022-05-10 16:11:35 +0200

    all data from Json showing up on screen, just need a way too see the input is equal to the heroes.name array somehow

--- 07-fetch/02-details/script.js
+++ 07-fetch/02-details/script.js
@@ -9,6 +9,39 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(() => {
-    // your code here
-})();
+// Loading.... looking for answers.... need to do an IF statement to compare variable hero = with the actual name of superhero in heroes.name array
+
+function fetchData(){
+    const hero = document.getElementById("hero-id").value;
+
+
+    fetch ("http://localhost:63342/index.html/_shared/api.json")
+        .then(response =>{
+            if (!response.ok){
+                throw Error("ERROR");
+            }
+            return response.json();
+        })
+        .then (data =>{
+        console.log(data.heroes);
+        const html = data.heroes.map(user =>{
+            return `
+                <div class="user"></div>
+                        <p>Name: ${user.name}</p>
+                        <p>AlterEgo: ${user.alterEgo}</p>
+                        <p>abilities: ${user.abilities}</p>
+                </div>`
+        })
+            .join(" ");
+            console.log(html);
+
+        document.querySelector('#target').innerHTML = html;
+
+    })
+        .catch (error => {
+            console.log(error);
+        });
+}
+
+fetchData();
+
