a2f437e907e5ee8c9b8df28883434134352cfcef
MichaelMontei <code-me@hotmail.com>
2022-05-10 11:57:59 +0200

    fetched the JSon file, had the wrong url at first using the localhost, changed it to the right one

---
+++ 07-fetch/01-list-to-console/script.js
@@ -0,0 +1,20 @@
+/* becode/javascript
+ *
+ * /09-fetch/01-list-to-console/script.js - 11.1: liste vers console
+ *
+ * coded by leny@BeCode
+ * started at 12/05/2019
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(() => {
+
+    //https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+    document.getElementById('run').addEventListener("click", ()=>{
+        fetch("http://localhost:63342/index.html/_shared/api.json")
+            .then(response => response.json)
+            .then (data => console.log(data));
+    })
+})();
