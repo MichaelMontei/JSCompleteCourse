4b5b4c2a8d681acf38c9198a9888dbfa5eb38d1c
MichaelMontei <code-me@hotmail.com>
2022-05-13 10:09:54 +0200

    made a function getHeroes to get all the data from the API and log it into the console. Made the arrays.length + 1 and by storing values, the new hero gets added to the array.

--- 07-fetch/04-add-Optional/script.js
+++ 07-fetch/04-add-Optional/script.js
@@ -10,5 +10,33 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+
+    const url = "../../_shared/api.json";
+    document.getElementById('run').addEventListener("click", ()=>{
+
+        const nameHero = document.getElementById("hero-name").value;
+        const alterEgo = document.getElementById("hero-alter-ego").value;
+        const power = document.getElementById("hero-powers").value;
+
+        async function getHeroes(){
+            let response = await fetch(url)
+            let data = await response.json();
+            console.log(data.heroes); //check if all data comes through
+            console.log(nameHero);
+            console.log(alterEgo);
+            console.log(power);
+
+            //make the array longer for adding a hero
+            let heroesLength = data.heroes.length +1;
+
+            //specify the elements and const of elements by above to add them on the list
+            let newHero = {id: heroesLength, nameHero: `${name}`, alterEgo: `${alterEgo}`, abilities: `${power}`};
+
+            //pushing the new hero into the array
+            data.heroes.push(newHero);
+            console.log(data.heroes);
+        }
+        getHeroes();
+
+    })
 })();
